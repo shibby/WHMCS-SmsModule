90c850c0b4e5d4950ea926b3a80253758137be47
Turgay <turgaycoskun@gmail.com>
2014-10-11 23:42:40 +0300

    Update invoicepaymentreminder_third.php
    Fatura numarası ve tutarı eklendi

--- modules/addons/aktuel_sms/hooks/invoicepaymentreminder_third.php
+++ modules/addons/aktuel_sms/hooks/invoicepaymentreminder_third.php
@@ -8,8 +8,8 @@ $hook = array(
     ),
     'type' => 'client',
     'extra' => '',
-    'defaultmessage' => 'Sayin {firstname} {lastname}, {duedate} son odeme tarihli gecikmis bir faturaniz bulunmaktadir. Detayli bilgi icin sitemizi ziyaret edin.',
-    'variables' => '{firstname}, {lastname}, {duedate}'
+    'defaultmessage' => 'Sayin {firstname} {lastname}, {duedate} son odeme tarihli, {invoiceid} numaralı,  {total} tutarındaki faturaniz ödenmemiştir. Detayli bilgi icin sitemizi ziyaret edin.',
+    'variables' => '{firstname}, {lastname}, {duedate}, {invoiceid}, {total}'
 );
 
 if(!function_exists('InvoicePaymentReminder_thirdoverdue')){
@@ -30,7 +30,7 @@ if(!function_exists('InvoicePaymentReminder_thirdoverdue')){
         }
 
         $userSql = "
-        SELECT a.duedate,b.id as userid,b.firstname,b.lastname,`c`.`value` as `gsmnumber` FROM `tblinvoices` as `a`
+        SELECT a.total,a.duedate,b.id as userid,b.firstname,b.lastname,`c`.`value` as `gsmnumber` FROM `tblinvoices` as `a`
         JOIN tblclients as b ON b.id = a.userid
         JOIN `tblcustomfieldsvalues` as `c` ON `c`.`relid` = `a`.`userid`
         JOIN `tblcustomfieldsvalues` as `d` ON `d`.`relid` = `a`.`userid`
@@ -45,12 +45,11 @@ if(!function_exists('InvoicePaymentReminder_thirdoverdue')){
         $num_rows = mysql_num_rows($result);
         if($num_rows == 1){
             $UserInformation = mysql_fetch_assoc($result);
-            
             $template['variables'] = str_replace(" ","",$template['variables']);
             $replacefrom = explode(",",$template['variables']);
-            $replaceto = array($UserInformation['firstname'],$UserInformation['lastname'],$class->changeDateFormat($UserInformation['duedate']));
+            $replaceto = array($UserInformation['firstname'],$UserInformation['lastname'],$class->changeDateFormat($UserInformation['duedate']),$args['invoiceid'],$UserInformation['total']);
             $message = str_replace($replacefrom,$replaceto,$template['template']);
-
+            
             $class->setGsmnumber($UserInformation['gsmnumber']);
             $class->setMessage($message);
             $class->setUserid($UserInformation['userid']);

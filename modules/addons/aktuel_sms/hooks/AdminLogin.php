3605315ec78f627e67e901a59f150dde2006c12d
TURGAY COŞKUN <adalim6161@users.noreply.github.com>
2014-10-24 22:59:28 +0300

    Create AdminLogin.php
    WHMCS admin paneline giriş yapıldığında yöneticiye mesaj atar

---
+++ modules/addons/aktuel_sms/hooks/AdminLogin.php
@@ -0,0 +1,39 @@
+<?php
+$hook = array(
+    'hook' => 'AdminLogin',
+    'function' => 'AdminLogin_admin',
+    'type' => 'admin',
+    'extra' => '',
+    'defaultmessage' => '{username}, Yonetici paneline giris yapti.',
+    'variables' => '{username}'
+);
+if(!function_exists('AdminLogin_admin')){
+    function AdminLogin_admin($args){
+        $class = new AktuelSms();
+        $template = $class->getTemplateDetails(__FUNCTION__);
+        if($template['active'] == 0){
+            return null;
+        }
+        $settings = $class->getSettings();
+        if(!$settings['api'] || !$settings['apiparams'] || !$settings['gsmnumberfield'] || !$settings['wantsmsfield']){
+            return null;
+        }
+        $admingsm = explode(",",$template['admingsm']);
+
+        $template['variables'] = str_replace(" ","",$template['variables']);
+        $replacefrom = explode(",",$template['variables']);
+        $replaceto = array($args['username']);
+        $message = str_replace($replacefrom,$replaceto,$template['template']);
+
+        foreach($admingsm as $gsm){
+            if(!empty($gsm)){
+                $class->setGsmnumber( trim($gsm));
+                $class->setUserid(0);
+                $class->setMessage($message);
+                $class->send();
+            }
+        }
+    }
+}
+
+return $hook;
